bbc4ec3a64a2e86e461e2904c48bb5c2f15e412f
saraswatigpt <sandeep21wadekar@gmail.com>
2025-07-18 08:57:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-## Hi there 👋
+ Hi There!
 
-<!--
+
 
 
 Here are some ideas to get you started:
@@ -13,4 +13,4 @@ Here are some ideas to get you started:
 -  How to reach me: ...
 -  Pronouns: ...
 -  Fun fact: ...
--->
+
